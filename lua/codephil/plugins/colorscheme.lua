9fcadd6f8a884e320b97843cb5b59387fce97ee0
codephilip <philip@meiers.in>
2024-04-28 21:38:11 +0200

    configure coloscheme and prefix whichkey

--- lua/codephil/plugins/colorscheme.lua
+++ lua/codephil/plugins/colorscheme.lua
@@ -13,23 +13,24 @@ return {
     local border = "#547998"
     require("tokyonight").setup({
       style = "night",
+      transparent = true,
       --style = "day",
       on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = bg_dark
-        colors.bg_float = bg_dark
+        colors.bg = "NONE"
+        --colors.bg_dark = bg_dark
+        --colors.bg_float = bg_dark
         colors.bg_highlight = bg_highlight
         colors.bg_popup = bg_dark
         colors.bg_search = bg_search
         colors.bg_sidebar = bg_dark
         colors.bg_statusline = bg_dark
-        colors.bg_visual = bg_visual
+        --colors.bg_visual = bg_visual
         colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
+        --colors.fg = fg
+        --colors.fg_dark = fg_dark
+        --colors.fg_float = fg
         colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
+        --colors.fg_sidebar = fg_dark
       end,
     })
 
--- lua/codephil/plugins/which-key.lua
+++ lua/codephil/plugins/which-key.lua
@@ -1,14 +1,38 @@
 return {
   "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
+  config = function()
     vim.o.timeout = true
-    vim.o.timeoutlen = 500
+    vim.o.timeoutlen = 500 -- Set delay before which-key popup shows
+    local wk = require("which-key")
+    wk.setup({}) -- initialize which-key
+    wk.register({
+      l = {
+        name = "+LazyGit", -- custom group name for which-key
+      },
+      c = {
+        name = "+CodeActions", -- custom group name for which-key
+      },
+      e = {
+        name = "+File Explorer", -- custom group name for which-key
+      },
+      h = {
+        name = "+Git", -- custom group name for which-key
+      },
+      m = {
+        name = "+Prettier", -- custom group name for which-key
+      },
+      s = {
+        name = "+Splits", -- custom group name for which-key
+      },
+      t = {
+        name = "+Tabs", -- custom group name for which-key
+      },
+      w = {
+        name = "+Workbook", -- custom group name for which-key
+      },
+      x = {
+        name = "+Trouble", -- custom group name for which-key
+      },
+    }, { prefix = "<leader>" }) -- set leader key
   end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    --TODO:: update this plugin so we can start working with NVIM more effectively.
-  },
 }
